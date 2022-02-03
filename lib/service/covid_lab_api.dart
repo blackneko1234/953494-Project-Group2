e2567689534a6ded5da13199be9aa2d8c4d3b50a
Prin Ritti-traiphop <prinr2481@gmail.com>
2022-02-03 22:30:12 +0700

    Can error can't use u.text

--- lib/service/covid_lab_api.dart
+++ lib/service/covid_lab_api.dart
@@ -5,5 +5,7 @@ class CovidLabApi {
   Future<http.Response> fetchCovidLab() async {
     return await http
         .get(Uri.https("covid-lab-data.pages.dev", "/latest.json"));
+    print(http.get(Uri.https("covid-lab-data.pages.dev", "/latest.json")));
   }
+  
 }
